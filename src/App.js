1999f7416821790dbb5851db27d292eb92503d0a
bishar08 <abubushayra@gmail.com>
2022-03-25 01:50:53 -0500

    finished the about part

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
+import About from './components/about/About'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   )
 }
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,23 +8,24 @@ const Hero = () => {
       <video autoPlay loop muted id="video">
         <source src={ImageVid} type="video/mp4" />
       </video>
+
       <div className="hero-text">
         <h1>Decentralized</h1>
         <h1>
-          <span className="blue">Trading </span> Protocol
+          <span className="blue">Trading </span>Protocol
         </h1>
         <p>
-          Guaranteed liquidity trading for millions of users and top Ethereum
+          Granteed liquidity tradingr millions of usersnd top Ethereum
           applications.
         </p>
         <div className="btn-group">
           <button className="btn">Use Defi</button>
           {/* <button className="btn btn-outline">Documentation</button> */}
-          <button className="btn btn-outline">FAQ</button>
+          <button className="btn btn-outline">FAQs</button>
         </div>
       </div>
       <div className="bottom-text">
-        <h2>Total Volume Secured: $42,104,783,662.47</h2>
+        <h2>Total Volume secured: $42,104,783,662.47</h2>
       </div>
     </div>
   )
