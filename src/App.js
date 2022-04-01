3d74627f70e9696ea0bd018f44e80282d03c898c
bishar08 <abubushayra@gmail.com>
2022-04-01 00:20:37 -0500

    add a new Subscribe component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import About from './components/about/About'
 import Developer from './components/developer/Developer'
+import Subscribe from './components/subscribe/Subscribe'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Developer />
+      <Subscribe />
     </div>
   )
 }
---
+++ src/components/subscribe/Subscribe.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Subscribe.css'
+
+const Subscribe = () => {
+  return (
+    <div className="subscribe">
+      <div className="content">
+        <h2>Join Our DeFi Community</h2>
+        <form
+          action="https://getform.io/f/1fedd5dc-8354-409a-a6c1-7e181d2ba1e5"
+          method="POST"
+        >
+          <div className="form-container display-col">
+            <input type="email" name="email" placeholder="Enter your email" />
+            <button className="btn">Sign Up</button>
+          </div>
+          <div className="form-container">
+            <input type="checkbox" />
+            <p>Yes, I agree to receive email communication from DeFi.</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Subscribe
