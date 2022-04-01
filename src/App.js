eea9cf683026ef7200214fc6c484aa3512f3e009
bishar08 <abubushayra@gmail.com>
2022-03-31 23:46:17 -0500

    add a new developer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import About from './components/about/About'
+import Developer from './components/developer/Developer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Developer />
     </div>
   )
 }
